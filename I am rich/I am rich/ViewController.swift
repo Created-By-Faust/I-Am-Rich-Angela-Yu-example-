26a081b6e6df2386157c309d1579b686322b027e
Faust <80778093+Created-By-Faust@users.noreply.github.com>
2022-07-12 11:09:15 +0300

    Add files via upload

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Дмитрий Емелин on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
